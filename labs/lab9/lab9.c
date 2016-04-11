4df25055e594be7051d620bebd930f9d0263e095
GiselleSerate <giselle.serate@gmail.com>
2016-04-11 12:14:23 -0700

    Added all files from lab9 and submitted. Pretty easy.

---
+++ labs/lab9/lab9.c
@@ -0,0 +1,41 @@
+// Giselle Serate
+// April 11, 2016
+// Removes filename from a URL. 
+
+#include <stdio.h>
+
+
+void remove_filename(char* url);
+
+
+int main() {
+
+	char url[] = "http://www.knking.com/index.html";
+
+	printf("%s\n", url);
+
+	remove_filename(url);
+
+	printf("%s\n", url);
+
+	return 0;
+
+}
+
+
+// takes in pointer, changes string to remove filename
+// Process: searches for end of string, then goes backward to first slash it can find counting from the end of the string, making null chars as it goes.
+void remove_filename(char* url) {
+	// I don't remember the string idiom, so I'm going to make it up. 
+	while(*url++); // Post-increments the pointer if the character is NOT the null character
+	// Now it is pointing past, so:
+	url--;
+
+	// Now we go backwards in search of the slash.
+	while(*url != '/') {
+		*url = '\0'; // it's a null now
+		url--; // go backward
+	};
+
+	*url = '\0'; // Get rid of that slash.
+}
